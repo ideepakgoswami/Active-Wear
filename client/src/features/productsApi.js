b9f0bac1aa48c0e349c5bcb8165c6cb187cf1bbd
Deepak <ideepakgoswami@protonmail.com>
2023-04-29 16:41:13 +0530

    changes

--- client/src/features/productsApi.js
+++ client/src/features/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const productsApi = createApi({
     reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://65.1.109.82:5000/api/data" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://65.1.109.82/api/data" }),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
             query: () => "products/AllProducts"
